b801969f25b71128e113873d2617c546cdcd2095
marseliine <78792953+marseliine@users.noreply.github.com>
2023-09-07 18:14:59 +0300

    Delete routes directory

--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const initAuthRoutes = require('express').Router();
-const { registerValidation, loginValidation } = require('../middlewares/validation');
-const { login, createUser } = require('../controllers/users');
-
-initAuthRoutes.post('/signin', loginValidation, login);
-initAuthRoutes.post('/signup', registerValidation, createUser);
-
-module.exports = initAuthRoutes;
